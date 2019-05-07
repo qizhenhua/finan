bfa8460b8412b555120a65862cdede42af9187d8
Qi ZhenHua <qizhenhua1976@163.com>
2019-05-07 23:06:13 +0800

    Update

--- getdata.py
+++ getdata.py
@@ -1,3 +1,7 @@
-mychoice="519019"
-df=DataAPI.FundNavGet(secID=u"",ticker=mychoice,dataDate=u"",beginDate=u"20160101",endDate=u"20190430",field=u"NAV",pandas="1")
-df.to_csv(mychoice+".csv",cloumns="",encoding="GBK")
+# www.uqer.io
+# login and "start research"
+
+fundlist=["000961","001593"]  # Put fundation list in this list
+for mychoice in fundlist:
+    df=DataAPI.FundNavGet(secID=u"",ticker=mychoice,dataDate=u"",beginDate=u"20160101",endDate=u"",field=u"endDate,NAV",pandas="1")
+    df.to_csv(mychoice+".csv",cloumns="",encoding="GBK")
