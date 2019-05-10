01d0d8ed00be21d544583dff17c5edc5696a6e0b
Qi ZhenHua <qizhenhua1976@163.com>
2019-05-11 01:19:07 +0800

    Add some function
    Add some function and adjust some code.

--- Investment.py
+++ Investment.py
@@ -14,8 +14,8 @@ class Investment(object):
         self.Datelist=[]
         self.NAVlist=[]
         self.Investlist=[]
+        self.tableNAV={}
         self.readfund(fundcode)
-        self.clearaccount()
 
     def readfund(self,filename):
         myfile = open("./data/" + filename + ".csv", "r")
@@ -27,6 +27,8 @@ class Investment(object):
             if c!='NAV':
                 self.NAVlist.append(float(c))
                 self.Datelist.append(b[1])
+        self.tableNAV=dict(zip(self.Datelist,self.NAVlist))
+        self.clearaccount()
 
     def clearaccount(self):
         self.Investlist = []
@@ -37,24 +39,90 @@ class Investment(object):
         for i in range(len(self.Datelist)):
             if date<=self.Datelist[i]:
                 return i
+        return i
+
+    def dateprevious(self,datetrade,previousdays):
+        """return a date list of previouse days of trade date, not include trade day"""
+        i = self.indexdate(datetrade)
+        j = i - previousdays
+        if j < 0:
+            j = 0
+        return self.Datelist[j:i]
+
+    def datenext(self,datetrade,nextdays):
+        i = self.indexdate(datetrade)
+        j = i + nextdays
+        if j > len(self.Datelist):
+            j = self.Datelist[-1]
+        return self.Datelist[i:j]
+
+    def dataperiod(self, datestart, dateend):
+        """retrun a dict of data of date/NAV/investmoney"""
+        i=self.indexdate(datestart)
+        j=self.indexdate(dateend) + 1
+        result = dict(zip(["date",             "NAV",             "investmoney"       ], \
+                          [self.Datelist[i:j], self.NAVlist[i:j], self.Investlist[i:j]]))
+        return result
+
+    def NAVaverage(self,datestart,dateend):
+        myNAVlist=self.dataperiod(datestart,dateend)["NAV"]
+        sumNAV=0.0
+        for price in myNAVlist:
+            sumNAV += price
+        return sumNAV/len(myNAVlist)
+
+    def rategrowth(self):
+        """calculate growth rate for each day, return a whole list"""
+        result=[0]
+        for i in range(1,len(self.NAVlist)):
+            a=(self.NAVlist[i]-self.NAVlist[i-1])/self.NAVlist[i-1]
+            result.append(a)
+        return result
+
+    def capitalinvested(self):
+        """calculate invested money of all"""
+        result=0.0
+        for i in self.Investlist:
+            result += i
+        return result
+
+    def sharesaved(self):
+        result=0.0
+        for i in zip(self.Investlist,self.NAVlist):
+            result += i[0]/i[1]
+        return result
+
+    def pricebalance(self):
+        return self.capitalinvested()/self.sharesaved()
 
     def buy(self,date,money):
         i=self.indexdate(date)
         self.Investlist[i] = self.Investlist[i] + money
 
     def sell(self,date):
-        indexend=self.indexdate(date)
-        price=self.NAVlist[indexend]
-        capital,share=0.0,0.0
-        for i in range(len(self.Datelist)):
-            if self.Investlist[i]>0:
-                capital += self.Investlist[i]
-                share += self.Investlist[i]/self.NAVlist[i]
+        price=self.tableNAV[date]
+        capital=self.capitalinvested()
+        share=self.sharesaved()
         money=share * price
-        rate=((money-capital)/capital)*100
-        return capital,money,rate
+        rate=(money-capital)/capital
+        result=dict(zip(["capital","money","rate","bonus"],\
+                        [capital,money,rate,money-capital]))
+        return result
+
+    def printinvestment(self,datestart,dateend=""):
+        if dateend == "":
+            dateend = self.Datelist[-1]
+        a=self.dataperiod(datestart,dateend)
+        dates=a["date"]
+        NAVs=a["NAV"]
+        money=a["investmoney"]
+        datas=list(zip(dates,NAVs,money))
+        for i in datas:
+            print(i)
 
     def investregular(self,datestart,dateend,moneyregular):
+        """The simplest investing mode, set start date / end date / money for each day, it will set money to the list
+        """
         istart=self.indexdate(datestart)
         iend=self.indexdate(dateend)
         i=0
@@ -64,17 +132,38 @@ class Investment(object):
             datetrade=self.Datelist[istart+i]
             i += 1
 
-    def investregulartarget(self,datestart,moneyregula,ratetarget,daysmin):
-        istart=self.indexdate(datestart)
-        i=0
-        datetrade=datestart
-        while datetrade < self.Datelist[-1]:
-            self.buy(datetrade,moneyregula)
-            datetrade=self.Datelist[istart+i]
-            i += 1
-            if i > daysmin and self.sell(datetrade)[2] >= ratetarget:
+    def investstrategy01(self,datestart,moneypiece,ratetarget,daysmin,):
+        days = 0
+        dateend=self.Datelist[-1]
+        mydatelist=self.dataperiod(datestart,dateend)["date"]
+        for datetrade in mydatelist:
+            self.buy(datetrade,moneypiece)
+            days += 1
+            if days> daysmin and self.sell(datetrade)["rate"] >= ratetarget:
                 break
         return datetrade
+
+    def investstrategy02(self,datestart,moneypiece,ratetarget,daysmin,ratelowerlimit=-100,factor=0):
+        days = 0
+        dateend=self.Datelist[-1]
+        mydatelist=self.dataperiod(datestart,dateend)["date"]
+        dateprevious=datestart
+        for datetrade in mydatelist:
+            self.buy(datetrade,moneypiece)
+            days += 1
+            if days> daysmin and self.sell(datetrade)["rate"] >= ratetarget:
+                break
+            temp=self.dateprevious(datetrade,3)
+            temp=self.NAVaverage(temp[0],temp[-1])
+            temp=(self.tableNAV[datetrade] - temp)/ temp
+            if  temp < ratelowerlimit: # and self.tableNAV[datetrade] < 1.45:
+                self.buy(datetrade,moneypiece*factor)
+                print(temp)
+            dateprevious=datetrade
+        return datetrade
+
+    def testing01(self):
+        pass
 
 a=Investment("001593")
 buydate="2018-01-04"
@@ -84,15 +173,25 @@ a.investregular(buydate,selldate,10)
 print(a.sell(selldate))
 a.clearaccount()
 
-b=a.investregulartarget(buydate,10,16,10)
+b=a.investstrategy01(buydate,10,0.10,10)
 print(a.sell(b))
 print(b)
+a.clearaccount()
 
+print("Strategy 2:")
+start=buydate
+for i in range(5):
+    b=a.investstrategy02(start,10,0.05,10,-0.03,100)
+    print(a.sell(b))
+    print(b)
+    start=a.datenext(b,2)[-1]
+    print("next day %s"%start)
 
+    #a.printinvestment(buydate,b)
+    a.clearaccount()
 
 #if you want to make it as a module, remove below code's #
 #def main():
 #
 #if __name__=="__main__":
 #    main()
-
